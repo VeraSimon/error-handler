0925ed68e4658f93b4a8a14563237a6f0d93714b
VeraSimon <vera.simon01@gmail.com>
2019-10-17 23:46:48 +0100

    Restructure Moved handlers and HTTP codes to respective directories Moved errors.js to iana.js and started adding rest of the documented HTTP codes

---
+++ errorHandler/codes/iana.js
@@ -0,0 +1,398 @@
+// https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
+
+// h###_#: {
+//    httpStatus: '###.#',
+//    title: '',
+//    description: '',
+//    recoveryInstructions: ''
+// }
+
+const iana = {
+   h100: {
+      httpStatus: '100',
+      title: 'Continue',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h101: {
+      httpStatus: '101',
+      title: 'Switching Protocols',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h102: {
+      httpStatus: '102',
+      title: 'Processing',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h103: {
+      httpStatus: '103',
+      title: 'Early Hints',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h200: {
+      httpStatus: '200',
+      title: 'OK',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h201: {
+      httpStatus: '201',
+      title: 'Created',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h202: {
+      httpStatus: '202',
+      title: 'Accepted',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h203: {
+      httpStatus: '203',
+      title: 'Non-Authoritative Information',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h204: {
+      httpStatus: '204',
+      title: 'No Content',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h205: {
+      httpStatus: '205',
+      title: 'Reset Content',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h206: {
+      httpStatus: '206',
+      title: 'Partial Content',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h207: {
+      httpStatus: '207',
+      title: 'Multi-Status',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h208: {
+      httpStatus: '208',
+      title: 'Already Reported',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h226: {
+      httpStatus: '226',
+      title: 'IM Used',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h300: {
+      httpStatus: '300',
+      title: 'Multiple Choices',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h301: {
+      httpStatus: '301',
+      title: 'Moved Permanently',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h302: {
+      httpStatus: '302',
+      title: 'Found',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h303: {
+      httpStatus: '303',
+      title: 'See Other',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h304: {
+      httpStatus: '304',
+      title: 'Not Modified',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h305: {
+      httpStatus: '305',
+      title: 'Use Proxy',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h306: {
+      httpStatus: '306',
+      title: '(Unused)',
+      description: 'This code is no longer used and has been reserved.',
+      recoveryInstructions:
+         'Please contact the Administrator about updating their code.'
+   },
+   h307: {
+      httpStatus: '307',
+      title: 'Temporary Redirect',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h308: {
+      httpStatus: '308',
+      title: 'Permanent Redirect',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h400: {
+      httpStatus: '400',
+      title: 'Bad Request',
+      description: 'All object properties must be provided',
+      recoveryInstructions:
+         'Please ensure all required object properties are in your request and try again.'
+   },
+   h401: {
+      httpStatus: '401',
+      title: 'Unauthorized',
+      description: 'The credentials provided were invalid',
+      recoveryInstructions:
+         'Please ensure you have entered valid credentials and try again.'
+   },
+   h402: {
+      httpStatus: '402',
+      title: 'Payment Required',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h403: {
+      httpStatus: '403',
+      title: 'Forbidden',
+      description: 'The request was valid, but the server is refusing action',
+      recoveryInstructions:
+         'The user might not have the necessary permissions for a resource, or may need an account of some sort.'
+   },
+   h404: {
+      httpStatus: '404',
+      title: 'Not Found',
+      description: 'The server was unable to find the specified object',
+      recoveryInstructions:
+         'Please ensure you specify an existing object and try again.'
+   },
+   h405: {
+      httpStatus: '405',
+      title: 'Method Not Allowed',
+      description:
+         'A request method is not supported for the requested resource',
+      recoveryInstructions:
+         'Please ensure your HTTP request is of the proper type and try again.'
+   },
+   h406: {
+      httpStatus: '406',
+      title: 'Not Acceptable',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h407: {
+      httpStatus: '407',
+      title: 'Proxy Authentication Required',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h408: {
+      httpStatus: '408',
+      title: 'Request Timeout',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h409: {
+      httpStatus: '409',
+      title: 'Conflict',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h410: {
+      httpStatus: '410',
+      title: 'Gone',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h411: {
+      httpStatus: '411',
+      title: 'Length Required',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h412: {
+      httpStatus: '412',
+      title: 'Precondition Failed',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h413: {
+      httpStatus: '413',
+      title: 'Payload Too Large',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h414: {
+      httpStatus: '414',
+      title: 'URI Too Long',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h415: {
+      httpStatus: '415',
+      title: 'Unsupported Media Type',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h416: {
+      httpStatus: '416',
+      title: 'Range Not Satisfiable',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h417: {
+      httpStatus: '417',
+      title: 'Expectation Failed',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h421: {
+      httpStatus: '421',
+      title: 'Misdirected Request',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h422: {
+      httpStatus: '422',
+      title: 'Unprocessable Entity',
+      description:
+         'The request was well-formed but was unable to be followed due to semantic errors.',
+      recoveryInstructions:
+         'Make sure your request includes all expected properties and try again.'
+   },
+   h423: {
+      httpStatus: '423',
+      title: 'Locked',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h424: {
+      httpStatus: '424',
+      title: 'Failed Dependency',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h425: {
+      httpStatus: '425',
+      title: 'Too Early',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h426: {
+      httpStatus: '426',
+      title: 'Upgrade Required',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h428: {
+      httpStatus: '428',
+      title: 'Precondition Required',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h429: {
+      httpStatus: '429',
+      title: 'Too Many Requests',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h431: {
+      httpStatus: '431',
+      title: 'Request Header Fields Too Large',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h451: {
+      httpStatus: '451',
+      title: 'Unavailable For Legal Reasons',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h500: {
+      httpStatus: '500',
+      title: 'Internal Server Error',
+      description:
+         'The server is having trouble communicating with the database',
+      recoveryInstructions:
+         'Please provide the server administrator the provided errorOutput and/or try again later.'
+   },
+   h501: {
+      httpStatus: '501',
+      title: 'Not Implemented',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h502: {
+      httpStatus: '502',
+      title: 'Bad Gateway',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h503: {
+      httpStatus: '503',
+      title: 'Service Unavailable',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h504: {
+      httpStatus: '504',
+      title: 'Gateway Timeout',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h505: {
+      httpStatus: '505',
+      title: 'HTTP Version Not Supported',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h506: {
+      httpStatus: '506',
+      title: 'Variant Also Negotiates',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h507: {
+      httpStatus: '507',
+      title: 'Insufficient Storage',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h508: {
+      httpStatus: '508',
+      title: 'Loop Detected',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h510: {
+      httpStatus: '510',
+      title: 'Not Extended',
+      description: '',
+      recoveryInstructions: ''
+   },
+   h511: {
+      httpStatus: '511',
+      title: 'Network Authentication Required',
+      description: '',
+      recoveryInstructions: ''
+   }
+};
+
+module.exports = {
+   iana
+};
---
+++ errorHandler/handlers/express.js
@@ -0,0 +1,68 @@
+// TODO: Refactor as an Express.js middleware handler using codes from /codes
+let isDotEnv = true;
+try {
+   require('dotenv').config();
+} catch (e) {
+   isDotEnv = false;
+   console.log('INFO: dotenv is not installed.');
+}
+
+const { errors } = require('./errors');
+
+const debugging = process.env.DEBUGGING.toLowerCase() === 'true' || false;
+
+// express.js middleware
+const errorHandler = (err, req, res, next) => {
+   // err = [status, message, {optional KVPs to add to req.body}]
+   // err[0] = HTTP status. Used in res.status().
+   // err[1] = Arbitrary message provided by the function call. Assigned to ErrorOutput key.
+   // Ex: next(["h404", "Oh no, your thing doesn't exist!"])
+   // err[2] = (Optionally) add arbitrary KVPs to the body of the response, in case an existing frontend is expecting them.
+   // Ex: next(["h500", "Database error.", {"message": "An unspecified database error ocurred."}])
+   const status = err[0];
+   const message = err[1];
+   const kvps = err[2] || null;
+
+   // ruh roh. "status" isn't in errors...
+   if (!errors.hasOwnProperty(status))
+      throw `Uncaught Exception! Please review:\n${err}`;
+
+   // continue as normal
+   if (!isDotEnv) {
+      if (status === 'h500') console.error('Error:\n', message);
+   } else {
+      if (status === 'h500' && debugging === true)
+         console.error('Error:\n', message);
+   }
+   const error = { ...errors[status], errorOutput: message };
+   if (kvps !== null && kvps !== undefined && typeof kvps === 'object') {
+      for (let key in kvps) {
+         error[key] = kvps[key];
+      }
+   }
+   res.status(error.httpStatus).json(error);
+};
+
+// TODO: Split this off to a seperate handler/file
+// just returns the JS object matching status with the specified message on errorOutput
+const statusObj = (status, message, kvps) => {
+   // status = http status
+   // message = arbitrary message provided by the function call
+   // kvps = Optional object of arbitrary key-value pairs to add to the returned error object
+   if (!errors.hasOwnProperty(status)) {
+      return { error: `HTTP status '${status}' not defined!`, status, message };
+   } else {
+      let error = { ...errors[status], errorOutput: message };
+      if (kvps !== null && kvps !== undefined && typeof kvps === 'object') {
+         for (let key in kvps) {
+            error[key] = kvps[key];
+         }
+      }
+      return error;
+   }
+};
+
+module.exports = {
+   errorHandler,
+   statusObj
+};
