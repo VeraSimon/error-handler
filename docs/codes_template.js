9d0b6ba137cd7fec1cbc2f5fb1a52b7021b06610
VeraSimon <vera.simon01@gmail.com>
2019-10-17 23:47:45 +0100

    Docs Added a codes template to docs/

---
+++ docs/codes_template.js
@@ -0,0 +1,12 @@
+const service_name = {
+   // h###_#: {
+   //    httpStatus: '###.#',
+   //    title: '',
+   //    description: '',
+   //    recoveryInstructions: ''
+   // }
+};
+
+module.exports = {
+   service_name
+};
